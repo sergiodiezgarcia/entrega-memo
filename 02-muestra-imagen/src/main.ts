3f5fdcc83b9d1d6f20a6f4099166836988ce81c0
Sergio Diez <diezgarciasergioo@gmail.com>
2024-05-12 20:25:11 +0200

    prueba imagen

--- 02-muestra-imagen/src/main.ts
+++ 02-muestra-imagen/src/main.ts
@@ -1,1 +1,9 @@
-console.log("HOLA !!");
+
+
+document.getElementById("imagenCarta")?.addEventListener("click", function (e ) {
+  console.log("Ha clickado");
+  //let imagen = document.getElementById("carta por delante")
+  if (e.target instanceof HTMLImageElement ){
+    e.target.src = "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/memo/1.png";
+  }
+});
