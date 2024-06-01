5a671b972f60b5f734274fbfcb27429dc8010a22
Sergio Diez <diezgarciasergioo@gmail.com>
2024-06-01 21:00:45 +0200

    refactor

--- 04-segunda-imagen/src/main.ts
+++ 04-segunda-imagen/src/main.ts
@@ -1,30 +1,27 @@
 let haclickado1 = false;
-
 let haclickado2 = false;
 
 document.getElementById("carta1")?.addEventListener("click", function () {
-  haclickado1 = true;
+  haclickado1 = !haclickado1;
   this.classList.toggle("volteada");
-  let imagen = document.getElementById("imgcarta1");
-  if (imagen instanceof HTMLImageElement) {
-    imagen.src =
-      "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/memo/1.png";
-  }
-
+  muestraCarta("imgcarta1");
   verificarCartas();
 });
 
 document.getElementById("carta2")?.addEventListener("click", function () {
-  haclickado2 = true;
+  haclickado2 = !haclickado2;
   this.classList.toggle("volteada");
-  let imagen = document.getElementById("imgcarta2");
+  muestraCarta("imgcarta2");
+  verificarCartas();
+});
+
+const muestraCarta = (id: string) => {
+  let imagen = document.getElementById(id);
   if (imagen instanceof HTMLImageElement) {
     imagen.src =
       "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/memo/1.png";
   }
-
-  verificarCartas();
-});
+};
 
 function verificarCartas() {
   if (haclickado1 && haclickado2) {
